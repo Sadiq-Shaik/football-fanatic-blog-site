2521483e00f14fdbe14cc8a2b106d37740f63c54
Sadiq-Shaik <sadiqmerajshaik@gmail.com>
2022-08-23 13:08:08 +0530

    Added responsive css

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,36 +4,73 @@ import classes from "./AboutMe.module.css";
 function AboutMe(props) {
   return (
     <div className={classes.abt_me}>
-      <h2>
+      <h3>
         Hi, I'm <span>Sadiq Shaik</span>
-      </h2>
+      </h3>
       <h3>Web Developer</h3>
       <p>
-        The bowl was filled with fruit. It seemed to be an overabundance of
-        strawberries, but it also included blueberries, raspberries, grapes, and
-        banana slices. This was the meal Sarah had every morning to start her
-        day since she could remember. Why she decided to add chocolate as an
-        option today was still a bit of a surprise, but she had been in the
-        process of deciding she wanted to change her routine. This was a baby
-        step to begin that start.
+        Born in mid 90's in the historical city of Rajamundry, Andhra Pradesh
+        and grew up ashtonished watching the technological development from CRT
+        television, Dumb phones to Personal Computers, the Internet and
+        Smartphones. Graduated B.Tech in Electronics and Communications
+        Engineering in 2017 from GMR Institute of Technology, Rajam affiliated
+        to University of JNTU, Kakinada. Fascinated with the rapid development
+        in world of web technologies brought by NodeJS, Single Page Web Apps and
+        API revolution, I decided to join this revolution of moving all
+        traditional businesses towards Automation, Internet of Things, App based
+        businesses as once said by Bill Gates
+        <span>
+          "If your business is not on the Internet, then your business will be
+          out of business."
+        </span>
       </p>
       <p>
-        Turning away from the ledge, he started slowly down the mountain,
-        deciding that he would, that very night, satisfy his curiosity about the
-        man-house. In the meantime, he would go down into the canyon and get a
-        cool drink, after which he would visit some berry patches just over the
-        ridge, and explore among the foothills a bit before his nap-time, which
-        always came just after the sun had walked past the middle of the sky. At
-        that period of the day the sun's warm rays seemed to cast a sleepy spell
-        over the silent mountainside, so all of the animals, with one accord,
-        had decided it should be the hour for their mid-day sleep.
+        And what better way than learning the most popular JavaScript library,
+        ReactJS followed by TypeScript and NextJS framework. I have learnt
+        JavaScript, ReactJS, Git version control, HTML 5 CSS 3 from the most
+        instructors on the whole Internet
+        <a
+          href="https://www.udemy.com/user/coltsteele/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Colt Steele
+        </a>
+        (Basic HTML CSS JavaScript NodeJS Express MongoDB),
+        <a
+          href="https://www.udemy.com/user/jonasschmedtmann/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jonas Schmedtmann
+        </a>
+        (Advanced HTML (5.0) CSS (3.0) SASS (SCSS) JavaScript (ES6+)),
+        <a
+          href="https://www.udemy.com/user/maximilian-schwarzmuller/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Maximilian Schwarzmüller
+        </a>
+        (ReactJS with hooks, react-router, redux, basics of NextJS)
+        <a href="/" target="_blank">
+          Read more about me, WEBSITE COMING SOON
+        </a>
       </p>
       <p>
-        It wasn't that he hated her. It was simply that he didn't like her much.
-        It was difficult for him to explain this to her, and even more difficult
-        for her to truly understand. She was in love and wanted him to feel the
-        same way. He didn't, and no matter how he tried to explain to her she
-        refused to listen or to understand.
+        This webiste is made possible by Firebase by Google using its Realtime
+        Database and Hosting Service which is priovided for free and built with
+        create react-app by Facebook, react-router-dom by Remix. Souce code is
+        available on GitHub here:
+        <a
+          href="https://github.com/Sadiq-Shaik/football-fanatic-blog-site"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Repo
+        </a>
+        For any improvements and suggestions, you can
+        <a href="/contact">cantact me.</a>
       </p>
       <GoToTop />
     </div>
